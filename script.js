8b365797b0635d8f1ddd293b296e78c8c48f75f4
CasteAwayy <140751958+CasteAwayy@users.noreply.github.com>
2024-12-20 20:21:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-//your JS code here. If required.
+// "use strict";
+
+const form = document.querySelector("form");
+const buttonExis = document.querySelector("#existing");
+
+if (localStorage.getItem("username")) {
+  buttonExis.classList.remove("hidden");
+}
+buttonExis.addEventListener("click", () => {
+  alert(`Logged in as ${localStorage.getItem("username")}`);
+});
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
+  const formData = new FormData(this);
+  console.log(formData);
+  const username = formData.get("name");
+  const password = formData.get("password");
+  if (formData.get("checkbox")) {
+    localStorage.setItem("username", username);
+    localStorage.setItem("password", password);
+  }
+  alert(`Logged in as ${username}`);
+});
